54adca2a5073478cbeecf5aeea2682793f0bb561
Harrish Suhumar <harrish.s@hotmail.ca>
2020-12-30 12:41:08 -0500

    item schema and crud functions

--- controllers/restaurant.js
+++ controllers/restaurant.js
@@ -1,3 +1,60 @@
-exports.getMenu = (req, res, next) => {
+const Item = require("../models/item");
 
+exports.getMenu = (req, res, next) => {
+    Item.find()
+    .then(items => {
+        res.status(200).json({items: items});
+    }).catch(err => res.status(400).json({err: err}));
+}
+
+exports.getItem = (req, res, next) => {
+    const itemId = req.params.itemId;
+    Item.findById(itemId)
+    .then(item => res.status(200).json({item: item}))
+    .catch(err => res.status(400).json({err: err}));
+}
+
+exports.postAddItem = (req, res, next) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+    const imageUrl = req.body.imageUrl;
+
+    const item = new Item(
+        {
+            name,
+            price,
+            description,
+            imageUrl
+        }
+    )
+    item.save()
+    .then(result => {
+        res.status(200).json({item: item});
+    }).catch(err => res.status(400).json({err: err}));
+}
+
+exports.putEditItem = (req, res, next) => {
+    const itemId = req.params.itemId;
+    const name = req.body.name;
+    const price = req.body.price;
+    const description = req.body.description;
+    const imageUrl = req.body.imageUrl;
+
+    Item.findById(itemId)
+    .then(item => {
+        item.name = name;
+        item.price = price;
+        item.description = description;
+        item.imageUrl = imageUrl;
+        return item.save()
+    }).then(result => res.status(200).json({item: result}))
+    .catch(err => res.status(400).json({err: err}));
+}
+
+exports.deleteItem = (req, res, next) => {
+    const itemId = req.params.itemId;
+    Item.findByIdAndDelete(itemId)
+    .then(result => res.status(200).json({item: "Item successfully deleted!"}))
+    .catch(err => res.status(400).json({err: err}));
 }
--- routes/restaurant.js
+++ routes/restaurant.js
@@ -6,4 +6,12 @@ const router = express.Router();
 
 router.get("/menu", restaurantController.getMenu);
 
+router.get("/menu/:itemId", restaurantController.getItem);
+
+router.post("/menu/add", restaurantController.postAddItem);
+
+router.put("/menu/edit/:itemId", restaurantController.putEditItem);
+
+router.delete("/menu/delete/:itemId", restaurantController.deleteItem);
+
 module.exports = router;
